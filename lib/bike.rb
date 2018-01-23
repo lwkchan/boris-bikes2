764c8dfe0f2ab6e00019dd1d620056d23d87e7cb
kotpsot <zygmunt.justyna@gmail.com>
2018-01-23 16:16:45 +0000

    add testing for new instances of Bike class

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,5 +1,4 @@
 class Bike
   def working?
-
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,4 +7,12 @@ require 'docking_station'
 
 describe DockingStation do # syntax for testing class instance - accepts class name
   it { is_expected.to respond_to :release_bike}
+  it 'releases working bikes' do
+    bike = subject.release_bike
+    expect(bike).to be_working
 end
+end
+
+# subject in line 11 is basically self. So we call method release_bike on class DockingStation.
+# this method - when called - returns new instance of class Bike.
+# we call it bike :P
