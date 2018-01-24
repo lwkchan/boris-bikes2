1e959ecdede5129c1b73d62466b548a3f733b129
Joshua Holloway <josh.holloway@live.co.uk>
2018-01-24 14:55:31 +0000

    New updates on q11

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,32 @@
 class DockingStation
+
+ # def initialize(bike, quantity)
+ #  @bike = bike
+ #  @quantity = quantity
+ #  end
+
+  attr_reader :bike
+
+  #same as
+  # def bike=(value)
+  # @age = value
+  #end
+
+  def show
+    puts @bike #(.working?)
+    puts @quantity
+  end
+
   def release_bike
     return Bike.new
   end
+
+  def dock(bike)
+
+  end
+
 end
+
+
+
+
